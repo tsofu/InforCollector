92ac7ff90f039acb76b6a40bd125f7cd5153266c
tsofu <tsofu@naver.com>
2014-09-02 01:26:12 +0900

    [updated] InforCollector.py and [added] iConfig.xml
    updated get prefix information
    added iConfig.xml

--- InforCollector.py
+++ InforCollector.py
@@ -16,39 +16,68 @@ import time
 import os
 from xml.etree.ElementTree import ElementTree
 
+g_config_file        = "\\iConfig.xml"
+g_clairview_server   = "\\ClairviewS.exe"
+g_clairview_iserver  = "\\ClairviewISP.exe"
+g_clairview_sclient  = "\\ClairviewSC.exe"
 
 def subStr(str, start, end):
     return str[start:end+1]
 
 
+##def loadTargetPathfromXmlfile():
+##    run_abspath = os.getcwd()
+##
+##    tmp_target_path = ""
+##    target_path = ""
+##    tree = ElementTree()
+###    tree.parse(run_abspath + "\\" + "iConfig.xml")
+##    tree.parse(run_abspath + g_config_file)
+##    root = tree.getroot()
+##    for child in root.iter("targetpath"):
+##        tmp_target_path = child.text
+##
+##    if (-1 != tmp_target_path.startswith("\\")):
+##        target_path = subStr(tmp_target_path, 0, len(tmp_target_path)-2)
+##    else:
+##        target_path = tmp_target_path
+##
+##    return target_path
+
 def loadTargetPathfromXmlfile():
     run_abspath = os.getcwd()
-
-    tmp_target_path = ""
-    target_path = ""
-    tree = ElementTree()
-    tree.parse(run_abspath + "\\" + "makezipfile.xml")
-    root = tree.getroot()
-    for child in root.iter("targetpath"):
-        tmp_target_path = child.text
-
-    if (-1 != tmp_target_path.startswith("\\")):
-        target_path = subStr(tmp_target_path, 0, len(tmp_target_path)-2)
-    else:
-        target_path = tmp_target_path
+    os.chdir("../")
+    target_path = os.getcwd()
 
     return target_path
 
 
+##def loadPrefixfromXmlfile():
+##    run_abspath = os.getcwd()
+##
+##    prefix = ""
+##    tree = ElementTree()
+###    tree.parse(run_abspath + "\\" + "iConfig.xml")
+##    tree.parse(run_abspath + g_config_file)
+##    root = tree.getroot()
+##    for child in root.iter("prefix"):
+##        prefix = child.text
+##
+##    return prefix
+
 def loadPrefixfromXmlfile():
     run_abspath = os.getcwd()
+    os.chdir("../")
+    target_path = os.getcwd()
 
-    prefix = ""
-    tree = ElementTree()
-    tree.parse(run_abspath + "\\" + "makezipfile.xml")
-    root = tree.getroot()
-    for child in root.iter("prefix"):
-        prefix = child.text
+    if True == os.path.isfile(target_path + g_clairview_server):
+        prefix = "ClairviewServer"
+    elif True == os.path.isfile(target_path + g_clairview_iserver):
+        prefix = "ClairviewIntegrationServer"
+    elif True == os.path.isfile(target_path + g_clairview_sclient):
+        prefix = "ClairviewSmartClient"
+    else:
+        prefix = "Clairview"
 
     return prefix
 
@@ -58,7 +87,8 @@ def loadItemfromXmlfile():
 
     target_path = loadTargetPathfromXmlfile()
     tree = ElementTree()
-    tree.parse(run_abspath + "\\" + "makezipfile.xml")
+#    tree.parse(run_abspath + "\\" + "iConfig.xml")
+    tree.parse(run_abspath + g_config_file)
     root = tree.getroot()
     list_items = []
     for child in root.iter("item"):
@@ -108,6 +138,16 @@ def makeZipFile():
 
 def main():
     print("process start to make zipfile of logfile +++++ [START]")
+
+    # exe path
+    run_abspath = os.getcwd()
+    os.chdir("../")
+    # work path
+    run_workpath = os.getcwd()
+
+    # 초기 실행 시 경로정보를 받아서 처리 해야 함
+    # 초기 경로정보 얻음 이후 처리 해야 함
+
     makeZipFile()
     print("process end to make zipfile of logfile ----- [END]")
     os.system("pause")
