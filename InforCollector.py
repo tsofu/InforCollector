6bad6fa82db04a87e54ada5e57611540ce5a0a91
tsofu <tsofu@naver.com>
2014-09-16 02:31:56 +0900

    [updated] complete to collect ClairviewVMS

--- InforCollector.py
+++ InforCollector.py
@@ -24,78 +24,38 @@ g_clairview_sclient  = "\\ClairviewSC.exe"
 def subStr(str, start, end):
     return str[start:end+1]
 
-
-##def loadTargetPathfromXmlfile():
-##    run_abspath = os.getcwd()
-##
-##    tmp_target_path = ""
-##    target_path = ""
-##    tree = ElementTree()
-###    tree.parse(run_abspath + "\\" + "iConfig.xml")
-##    tree.parse(run_abspath + g_config_file)
-##    root = tree.getroot()
-##    for child in root.iter("targetpath"):
-##        tmp_target_path = child.text
-##
-##    if (-1 != tmp_target_path.startswith("\\")):
-##        target_path = subStr(tmp_target_path, 0, len(tmp_target_path)-2)
-##    else:
-##        target_path = tmp_target_path
-##
-##    return target_path
-
-def loadTargetPathfromXmlfile():
-    run_abspath = os.getcwd()
-    os.chdir("../")
-    target_path = os.getcwd()
-
-    return target_path
+################################################################################
 
 
-##def loadPrefixfromXmlfile():
-##    run_abspath = os.getcwd()
-##
-##    prefix = ""
-##    tree = ElementTree()
-###    tree.parse(run_abspath + "\\" + "iConfig.xml")
-##    tree.parse(run_abspath + g_config_file)
-##    root = tree.getroot()
-##    for child in root.iter("prefix"):
-##        prefix = child.text
-##
-##    return prefix
-
-def loadPrefixfromXmlfile():
-    run_abspath = os.getcwd()
-    os.chdir("../")
-    target_path = os.getcwd()
-
-    if True == os.path.isfile(target_path + g_clairview_server):
-        prefix = "ClairviewServer"
-    elif True == os.path.isfile(target_path + g_clairview_iserver):
-        prefix = "ClairviewIntegrationServer"
-    elif True == os.path.isfile(target_path + g_clairview_sclient):
-        prefix = "ClairviewSmartClient"
+################################################################################
+# def loadPrefixfromExefile(run_workpath):
+################################################################################
+def loadPrefixfromExefile(run_workpath):
+    if True == os.path.isfile(run_workpath + g_clairview_server):
+        prefix = "[ClairviewVMS]Server"
+    elif True == os.path.isfile(run_workpath + g_clairview_iserver):
+        prefix = "[ClairviewVMS]IntegrationServer"
+    elif True == os.path.isfile(run_workpath + g_clairview_sclient):
+        prefix = "[ClairviewVMS]SmartClient"
     else:
-        prefix = "Clairview"
+        prefix = "[ClairviewVMS]Clairview"
 
     return prefix
 
 
-def loadItemfromXmlfile():
-    run_abspath = os.getcwd()
-
-    target_path = loadTargetPathfromXmlfile()
+################################################################################
+# def loadItemfromXmlfile(run_exepath):
+################################################################################
+def loadItemfromXmlfile(run_exepath, run_workpath):
     tree = ElementTree()
-#    tree.parse(run_abspath + "\\" + "iConfig.xml")
-    tree.parse(run_abspath + g_config_file)
+    tree.parse(run_exepath + g_config_file)
     root = tree.getroot()
     list_items = []
     for child in root.iter("item"):
-        list_items.append(target_path + "\\" + child.text)
+        list_items.append(run_workpath + "\\" + child.text)
+        print("item : " + child.text)
 
     return list_items
-
 
 #def checkFolder(target_path, list_folder_name):
 #    is_check = False
@@ -105,27 +65,28 @@ def loadItemfromXmlfile():
 #    return is_check
 
 
+################################################################################
+# def makeFileName(prefix):
+################################################################################
 def makeFileName(prefix):
     cur_time = time.localtime()
-    file_name = "%s_%04d%02d%02d_%02d%02d%02d.zip" % (prefix, cur_time.tm_year, cur_time.tm_mon, cur_time.tm_mday, cur_time.tm_hour, cur_time.tm_min, cur_time.tm_sec)
+    file_name = "%sLog_%04d%02d%02d_%02d%02d%02d.zip" % (prefix, cur_time.tm_year, cur_time.tm_mon, cur_time.tm_mday, cur_time.tm_hour, cur_time.tm_min, cur_time.tm_sec)
     return file_name
 
 
-def makeZipFile():
-    prefix      = loadPrefixfromXmlfile()
-    list_items  = loadItemfromXmlfile()
+################################################################################
+# def collectFileToZipfile(run_exepath, output_file_name):
+################################################################################
+def collectFileToZipfile(run_exepath, run_workpath, output_file_name):
+    list_items = loadItemfromXmlfile(run_exepath, run_workpath)
 
 #    is_check    = checkFolder(target_path, list_items)
 #    if (False == is_check):
 #        print("Don't search folder, please check folder")
 #        return
 
-    run_abspath = os.getcwd()
-    make_file_name = makeFileName(prefix)
-#   output_path = os.getcwd()
-    file_path = run_abspath + "\\" + make_file_name
-    print("ZipFile Infor : " + file_path)
-    f = zipfile.ZipFile(file_path, 'w', zipfile.ZIP_DEFLATED, True)
+    print("ZipFile Infor : " + output_file_name)
+    f = zipfile.ZipFile(output_file_name, 'w', zipfile.ZIP_DEFLATED, True)
 
     for item_log_folder in list_items:
         start_dir = item_log_folder
@@ -136,20 +97,31 @@ def makeZipFile():
     f.close()
 
 
+################################################################################
+# def main():
+################################################################################
 def main():
-    print("process start to make zipfile of logfile +++++ [START]")
+    print("Start to collect logfiles +++++ [START]")
+
+    userhome     = os.path.expanduser('~')
+    desktop_path = userhome + "\\Desktop"
 
     # exe path
-    run_abspath = os.getcwd()
+    run_exepath = os.getcwd()
     os.chdir("../")
     # work path
     run_workpath = os.getcwd()
 
-    # 초기 실행 시 경로정보를 받아서 처리 해야 함
-    # 초기 경로정보 얻음 이후 처리 해야 함
+    prefix           = loadPrefixfromExefile(run_workpath)
+    file_name        = makeFileName(prefix)
+    output_file_name = desktop_path + "\\" + file_name
 
-    makeZipFile()
-    print("process end to make zipfile of logfile ----- [END]")
+    collectFileToZipfile(run_exepath, run_workpath, output_file_name)
+
+    print("End to collect logfiles ----- [END]")
+    print("================================================================")
+    print("Complete to collect logfiles.!!!")
+    print("================================================================")
     os.system("pause")
 
 if __name__ == '__main__':
